8c56e87d812562e3c767c0e2de3923b7cf61d5fc
Matteo Meneghetti <matteomeneghetti@outlook.com>
2018-10-19 13:33:30 +0200

    Added asmx86 Hello world

---
+++ asm/hello.asm
@@ -0,0 +1,18 @@
+section .data
+string:     db 'Hello world!', 0Ah
+string_len: equ $ - str
+
+section .text                        
+global _start
+
+_start:
+	mov	eax, 4
+	mov	ebx, 1
+
+	mov	ecx, string
+	mov	edx, string_len
+	int	80h
+
+	mov	eax, 1
+	mov	ebx, 0
+	int	80h
